86616d106e570a28bbef51cdc33862070aa32cdf
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-12 20:13:06 +0000

    New project, plus placeholder for first week

---
+++ 2019_q3_coursework/src/pkg2019_q3_coursework/week_1/Solution.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2019_q3_coursework.week_1;
+
+/**
+ *
+ * @author charlie
+ */
+public class Solution {
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
