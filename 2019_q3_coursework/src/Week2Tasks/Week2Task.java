5a006f8d6ce12493d444a55c2bfad7f7dbe31d29
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-13 15:19:26 +0000

    Completed the calculating the area of a rectangle task

--- 2019_q3_coursework/src/Week2Tasks/Week2Task.java
+++ 2019_q3_coursework/src/Week2Tasks/Week2Task.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class Week2Task {
     public static void main(String[] args) {
         int counter = 10;
-        
+
         while(counter > 0)
         {
             System.out.print(counter);
@@ -20,17 +20,39 @@ public class Week2Task {
         }
         
         System.out.println("Please enter the letter that corresponds with your choice: ");
-        System.out.println("A: Calculate the area of a rectangle");
-        System.out.println("B: Display a multiplication table");
-        System.out.println("C: Add two numbers together");
+        System.out.println("1: Calculate the area of a rectangle");
+        System.out.println("2: Display a multiplication table");
+        System.out.println("3: Add two numbers together");
         Scanner scanIn = new Scanner(System.in);
-        String choice = scanIn.nextLine();
+        int choice = Integer.parseInt(scanIn.nextLine());
         
-        if(choice == "A")
+        if(choice == 1)
         {
-            System.out.println("Please enter the width: ");
+            System.out.println("You have selected 1: Calculate the area of a rectangle ");
+            System.out.println("Please enter the width of the rectangle: ");
             int width = Integer.parseInt(scanIn.nextLine());
+            
+             System.out.println("Please enter the height of the rectangle: ");
+            int height = Integer.parseInt(scanIn.nextLine());
+            
+            System.out.println("The area of the rectangle is: " + width * height);
+            
         }
         
+        else if(choice == 2)
+        {
+            
+        }
+        
+        else if(choice == 3) 
+        {
+            
+        }
+        else
+        {
+            System.out.println("I'm sorry that was not one of the options");
+        }
+        
+        scanIn.close();
     }
 }
