79769b84910b3704b20ab545c5ee535c23ed1175
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-13 09:54:31 +0000

    Create Week113thJan2020.java

---
+++ 2019_q3_coursework/Week1 13th Jan 2020/src/week1/pkg13th/jan/pkg2020/Week113thJan2020.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1.pkg13th.jan.pkg2020;
+
+/**
+ *
+ * @author charlie
+ */
+public class Week113thJan2020 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        System.out.println("My name is Charlie");
+        System.out.println("_______");
+        System.out.println("| o o |");
+        System.out.println("| --- |");
+        System.out.println("-------");
+    }
+    
+}
