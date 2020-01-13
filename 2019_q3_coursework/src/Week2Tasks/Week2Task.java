1793cfc847fbe272082e11de24cac2fc49c852c9
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-13 11:06:28 +0000

    Finished counting task

---
+++ 2019_q3_coursework/src/Week2Tasks/Week2Task.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week2Tasks;
+
+/**
+ *
+ * @author charlie
+ */
+public class Week2Task {
+    public static void main(String[] args) {
+        int counter = 10;
+        
+        while(counter > 0)
+        {
+            System.out.println(counter);
+            counter--;
+        }
+    }
+}
