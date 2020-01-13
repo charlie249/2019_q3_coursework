17f50cfb262d39105a471a92af8d4f5b9fc6c0cc
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-01-13 09:41:34 +0000

    Update Solution.java

--- 2019_q3_coursework/src/pkg2019_q3_coursework/week_1/Solution.java
+++ 2019_q3_coursework/src/pkg2019_q3_coursework/week_1/Solution.java
@@ -15,5 +15,6 @@ public class Solution {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("My name is Charlie");
     }
 }
